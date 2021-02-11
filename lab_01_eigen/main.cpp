a25b0b8717601450633e6e318f776a312c44db9c
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2021-02-11 19:47:13 +0100

    wo

--- lab_01_eigen/main.cpp
+++ lab_01_eigen/main.cpp
@@ -12,46 +12,112 @@ int main()
   //--------------------------------------
   // Todo: Create vector t.
   Eigen::Vector3d t;
+  t << 1, 0, 3; 
 
   // Todo: Create matrix A.
   Eigen::Matrix3d A;
+  A << 1, 0, 3, 
+       4, 5, 6, 
+       7, 8, 9; 
 
   // Todo: Create identity matrix I.
   Eigen::Matrix3d I;
+  I = Eigen::Matrix3d::Identity();
 
   // Todo: Create matrix T.
   Eigen::Matrix4d T;
+  T << A, t, 
+      0, 0, 0, 1; 
 
   // Todo: Create matrix B.
   Eigen::Matrix3d B;
+  B = A.transpose(); 
 
   // Print the results (uncomment code below).
-//  std::cout << "a) Create a few vectors and matrices:" << std::endl;
-//  std::cout << "-------------------------------------" << std::endl;
-//  std::cout << "t = " << std::endl << t << std::endl << std::endl;
-//  std::cout << "A = " << std::endl << A << std::endl << std::endl;
-//  std::cout << "I = " << std::endl << I << std::endl << std::endl;
-//  std::cout << "T = " << std::endl << T << std::endl << std::endl;
-//  std::cout << "B = " << std::endl << B << std::endl << std::endl;
+ std::cout << "a) Create a few vectors and matrices:" << std::endl;
+ std::cout << "-------------------------------------" << std::endl;
+ std::cout << "t = " << std::endl << t << std::endl << std::endl;
+ std::cout << "A = " << std::endl << A << std::endl << std::endl;
+ std::cout << "I = " << std::endl << I << std::endl << std::endl;
+ std::cout << "T = " << std::endl << T << std::endl << std::endl;
+ std::cout << "B = " << std::endl << B << std::endl << std::endl;
 
 
   // b) Coefficients.
   //-----------------
   // Todo: Follow lab notes.
+  std::cout << "Subtask b), changing coefficients\n"; 
+  std::cout << "t = " << std::endl << t << std::endl << std::endl;
+  std::cout << "A = " << std::endl << A << std::endl << std::endl; 
 
+  t(1) = 2; 
+  A(0, 1) = 2; 
+  std::cout << "t_2 = 2, a_1,2 = 2\n"; 
+  std::cout << "t = " << std::endl << t << std::endl << std::endl;
+  std::cout << "A = " << std::endl << A << std::endl << std::endl; 
+  std::cout << "T = [A, t ; 0, 1]\n"; 
+  T.block(0, 0, 3, 3) = A; 
+  T.block(0, 3, 3, 1) = t;
+  std::cout << "T = " << std::endl << T << std::endl << std::endl;
 
   // c) Block operations.
   //---------------------
   // Todo: Follow lab notes.
+  Eigen::Vector3d r_2 = A.row(1);  
 
+  Eigen::Vector3d c_2 = A.col(1); 
+
+  Eigen::Matrix<double, 3, 4> T_3x4 = T.block(0, 0, 3, 4); 
+
+  A.row(1).setConstant(0); 
+  A.col(1).setConstant(0); 
+  T.block(0, 0, 3, 4).setConstant(0); 
+
+  std::cout << "Subtask c), subvectors and submatrices\n"; 
+  std::cout << "A row 2 = " << std::endl << r_2 << std::endl << std::endl;
+  std::cout << "A col 2 = " << std::endl << c_2 << std::endl << std::endl;
+  std::cout << "A_3x4 = " << std::endl << T_3x4 << std::endl << std::endl;
+  std::cout << "Zeroing out these in the original...\n"; 
+  std::cout << "A = " << std::endl << A << std::endl << std::endl;
+  std::cout << "T = " << std::endl << T << std::endl << std::endl;  
 
   // d) Matrix and vector arithmetic.
   //---------------------------------
   // Todo: Follow lab notes.
+  std::cout << "Subtask d) Matrix and vector arithmetic\n"; 
+  std::cout << "Summing " << r_2.transpose() << " and " << c_2.transpose() << '\n'; 
+  std::cout << r_2 + c_2 << '\n'; 
 
+  std::cout << "Summing \n" << A << "\nand\n" << B << '\n'; 
+  std::cout << A + B << '\n'; 
+
+  std::cout << "Multiplying \n" << A << "\nand\n" << B << '\n';   
+  std::cout << A * B << '\n'; 
+
+  std::cout << "Dot product between " << r_2.transpose() << " and " << c_2.transpose() << '\n'; 
+  std::cout << r_2.dot(c_2) << '\n'; 
+
+  std::cout << "Coefficient-wisely multiplying \n" << A << "\nand\n" << B << '\n';   
+  std::cout << A.array() * B.array() << '\n'; 
 
   // e) Reductions.
   //---------------
   // Todo: Follow lab notes.
+  std::cout << "Subtask d) Reductions\n"; 
 
+  A(2, 2) = -A(2, 2); 
+  std::cout << "Sum of elements in \n" << A << "\n = " << A.sum() << '\n'; 
+
+  std::cout << "Minimum value and its position in \n" << A << '\n'; 
+  int row, col; 
+  double value = A.minCoeff(&row, &col); 
+  std::cout << "Value = " << value << " at row " << row << " and col " << col << '\n'; 
+
+  Eigen::Vector3d colMaxVector = A.colwise().maxCoeff(); 
+  std::cout << "Max coefficient vector of columns of A = " << colMaxVector << '\n'; 
+
+  std::cout << "L2-norm of " << colMaxVector << " is " << colMaxVector.squaredNorm() << '\n';   
+
+  Eigen::Vector3i boolVector = (colMaxVector.array() > 2).matrix().cast<int>(); 
+  std::cout << "Number of elements in " << colMaxVector.transpose() << " greater than 2 is " << boolVector.sum() << '\n'; 
 }
--- lab_01_transformations/main.cpp
+++ lab_01_transformations/main.cpp
@@ -1,4 +1,5 @@
 #include "Eigen/Eigen"
+#include "Eigen/Geometry"
 #include "opencv2/core/eigen.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgcodecs.hpp"
@@ -21,18 +22,22 @@ int main()
   //------------------------------------------
   // Todo: Translation.
   Eigen::Vector2d t;
+  t << 10, 15; 
 
   // Todo: Rotation (tip: take a look at https://eigen.tuxfamily.org/dox/group__Geometry__Module.html).
-  Eigen::Matrix2d R;
+  Eigen::Matrix2d R = Eigen::Rotation2Dd(deg2rad(20)).matrix();
 
   // Todo: Euclidean transformation that rotates and then translates.
   Eigen::Matrix3d E;
+  E << R, t, 
+       0, 0, 1; 
 
   std::cout << "Euclidean transformation E = " << std::endl << E << std::endl << std::endl;
 
   // Todo: Perform the transformation on a pixel u.
   Eigen::Vector2d u;
-  Eigen::Vector2d u_transformed;
+  u << 10, 5; 
+  Eigen::Vector2d u_transformed = (E * u.homogeneous()).hnormalized();
 
   std::cout << "Original pixel u = " << std::endl << u << std::endl << std::endl;
   std::cout << "Transformed pixel u_transformed = " << std::endl << u_transformed << std::endl << std::endl;
@@ -41,7 +46,7 @@ int main()
   // 2. Eigen and OpenCV.
   //---------------------
   cv::Mat E_cv;
-
+  cv::eigen2cv(E, E_cv); 
   // Todo: Convert from Eigen matrix to OpenCV cv::Mat.
 
   std::cout << "Euclidean transformation E_cv = " << std::endl << E_cv << std::endl << std::endl;
@@ -50,12 +55,26 @@ int main()
   // 3. Transform images.
   //---------------------
   // Todo: Read the image.
+  cv::Mat img_orig = cv::imread("../lab_01_transformations/img_grid.png");
+  if (img_orig.empty()) 
+  {
+    std::cerr << "Could not find image file\n"; 
+    return EXIT_FAILURE; 
+  }
 
   // Todo: Display the original image.
+  std::string orig_win_title{"Lab 1.2: Original image"}; 
+  cv::namedWindow(orig_win_title, cv::WINDOW_NORMAL); 
+  cv::imshow(orig_win_title, img_orig); 
 
   // Todo: Perform transformation on the image.
+  cv::Mat img_trans_E; 
+  cv::warpPerspective(img_orig, img_trans_E, E_cv, img_orig.size(), cv::INTER_CUBIC); 
 
   // Todo: Display the transformed image.
+  std::string trans_win_title{"Lab 1.2: Transformed image"}; 
+  cv::namedWindow(trans_win_title, cv::WINDOW_NORMAL); 
+  cv::imshow(trans_win_title, img_trans_E); 
 
 
   // 4. Composing transformations.
@@ -64,6 +83,6 @@ int main()
 
 
   // Please press a key, don't close the windows with your mouse.
-  cv::waitKey();
+  while (cv::waitKey(10) != 'q');
   return EXIT_SUCCESS;
 }
